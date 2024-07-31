51924a5d107c778d6d663b3271c6f32be77c66e6
Rohit Tyagi <rohit.tyagi@vwo.com>
2024-07-31 19:27:03 +0530

    change indentation

--- src/Service/SettingsService.php
+++ src/Service/SettingsService.php
@@ -11,48 +11,48 @@ use Drupal\Core\Config\ConfigFactoryInterface;
  */
 class SettingsService {
 
-    protected $configFactory;
+  protected $configFactory;
 
-    /**
-     * Constructs a new SettingsService object.
-     *
-     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-     *   The config factory.
-     */
-    public function __construct(ConfigFactoryInterface $config_factory) {
-        $this->configFactory = $config_factory;
-    }
+  /**
+   * Constructs a new SettingsService object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
 
-    /**
-     * Gets the VWO settings.
-     *
-     * @return array
-     *   An array of VWO settings.
-     */
-    public function getSettings() {
-        $config = $this->configFactory->get('vwo.settings');
-        return [
-            'id' => $config->get('id'),
-            'loading.type' => $config->get('loading.type'),
-            'loading.timeout.library' => $config->get('loading.timeout.library'),
-            'loading.timeout.settings' => $config->get('loading.timeout.settings'),
-            'loading.usejquery' => $config->get('loading.usejquery'),
-        ];
-    }
+  /**
+   * Gets the VWO settings.
+   *
+   * @return array
+   *   An array of VWO settings.
+   */
+  public function getSettings() {
+    $config = $this->configFactory->get('vwo.settings');
+    return [
+      'id' => $config->get('id'),
+      'loading.type' => $config->get('loading.type'),
+      'loading.timeout.library' => $config->get('loading.timeout.library'),
+      'loading.timeout.settings' => $config->get('loading.timeout.settings'),
+      'loading.usejquery' => $config->get('loading.usejquery'),
+    ];
+  }
 
-    /**
-     * Sets the VWO settings.
-     *
-     * @param array $values
-     *   An array of values to set.
-     */
-    public function setSettings(array $values) {
-        $config = $this->configFactory->getEditable('vwo.settings');
-        $config->set('id', $values['id'])
-            ->set('loading.type', $values['loading.type'])
-            ->set('loading.timeout.library', $values['loading.timeout.library'])
-            ->set('loading.timeout.settings', $values['loading.timeout.settings'])
-            ->set('loading.usejquery', $values['loading.usejquery'])
-            ->save();
-    }
+  /**
+   * Sets the VWO settings.
+   *
+   * @param array $values
+   *   An array of values to set.
+   */
+  public function setSettings(array $values) {
+    $config = $this->configFactory->getEditable('vwo.settings');
+    $config->set('id', $values['id'])
+      ->set('loading.type', $values['loading.type'])
+      ->set('loading.timeout.library', $values['loading.timeout.library'])
+      ->set('loading.timeout.settings', $values['loading.timeout.settings'])
+      ->set('loading.usejquery', $values['loading.usejquery'])
+      ->save();
+  }
 }
